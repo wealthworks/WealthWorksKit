c544953e9d653c60595dbaf0775037e4a225ed29
guoyalun <guoyalun@talicai.com>
2016-08-22 10:17:25 +0800

    修改错误信息处理

--- WealthWorksKit/Classes/Network/WWKHTTPSessionManager.m
+++ WealthWorksKit/Classes/Network/WWKHTTPSessionManager.m
@@ -94,5 +94,74 @@
     
 }
 
+#pragma mark - Override
+- (NSURLSessionDataTask *)dataTaskWithHTTPMethod:(NSString *)method
+                                       URLString:(NSString *)URLString
+                                      parameters:(id)parameters
+                                  uploadProgress:(nullable void (^)(NSProgress *uploadProgress)) uploadProgress
+                                downloadProgress:(nullable void (^)(NSProgress *downloadProgress)) downloadProgress
+                                         success:(void (^)(NSURLSessionDataTask *, id))success
+                                         failure:(void (^)(NSURLSessionDataTask *, NSError *))failure
+{
+    NSError *serializationError = nil;
+    NSMutableURLRequest *request = [self.requestSerializer requestWithMethod:method URLString:[[NSURL URLWithString:URLString relativeToURL:self.baseURL] absoluteString] parameters:parameters error:&serializationError];
+    if (serializationError) {
+        if (failure) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wgnu"
+            dispatch_async(self.completionQueue ?: dispatch_get_main_queue(), ^{
+                failure(nil, serializationError);
+            });
+#pragma clang diagnostic pop
+        }
+        
+        return nil;
+    }
+    
+    __block NSURLSessionDataTask *dataTask = nil;
+    dataTask = [self dataTaskWithRequest:request
+                          uploadProgress:uploadProgress
+                        downloadProgress:downloadProgress
+                       completionHandler:^(NSURLResponse * __unused response, id responseObject, NSError *error) {
+                           if (error) {
+                               if (failure) {
+                                   NSError *msg = [self getErrorMessage:responseObject];
+                                   failure(dataTask, msg!=nil?msg:error);
+                               }
+                           } else {
+                               if (success) {
+                                   success(dataTask, responseObject);
+                               }
+                           }
+                       }];
+    
+    return dataTask;
+}
+
+#pragma mark - Private
+- (NSError *)getErrorMessage:(NSDictionary *)responseObject {
+    
+    NSDictionary *messages = [responseObject objectForKey:@"messages"];
+    NSString *errorMessage = nil;
+    if ([messages count] > 0) {
+        NSMutableArray *array = [NSMutableArray array];
+        for (NSArray *values in messages.allValues) {
+            [array addObject:[values componentsJoinedByString:@"\n"]];
+        }
+        errorMessage = [array componentsJoinedByString:@"\n"];
+    } else if (messages == nil) {
+        errorMessage = [responseObject objectForKey:@"message"];
+    }
+    
+    if(errorMessage) {
+        NSNumber *codeNumber = responseObject[@"code"];
+        NSError *error = [NSError errorWithDomain:@"WWKErrorDomainAFNetworking" code:[codeNumber integerValue] userInfo:@{NSLocalizedDescriptionKey: errorMessage}];
+        return error;
+    }
+    
+    return nil;
+    
+}
+
 
 @end
